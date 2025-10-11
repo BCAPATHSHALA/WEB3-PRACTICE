b97d8590642081eef7bff2cf5f49cf028540c870
BCAPATHSHALA <manojkumargfg.1@gmail.com>
2025-10-11 10:06:13 +0530

    types: create the file for types of wallet

---
+++ web3wallet/types/types.wallet.ts
@@ -0,0 +1,22 @@
+export interface IMainRecoveryPhrase {
+  mnemonicPhrase: string;
+  masterSeedPhrase: string;
+}
+
+export interface IWallet {
+  path: string;
+  derivedSeed: string;
+  privateKey: string;
+  publicKey: string;
+  coinType: string;
+  accountNumber: string;
+}
+
+export type StorageValue =
+  | string
+  | number
+  | boolean
+  | object
+  | null
+  | IMainRecoveryPhrase
+  | IWallet;
